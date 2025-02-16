e9d0911caac0410efa8f042375a713ef339246b2
Bernhard Wilson <wilson.protected@gmail.com>
2025-02-16 16:08:45 +0200

    update tabs (worked by alpine)

--- src/resources/views/components/tab.blade.php
+++ src/resources/views/components/tab.blade.php
@@ -1,5 +1,7 @@
-<div {{ $attributes->merge([
-    'class' => 'flex flex-row justify-around flex-auto items-end border-b border-section-separator text-sm text-subtitle overflow-x-auto overflow-y-hidden'
-]) }}>
+<div
+    x-data="{ active: null }"
+    x-modelable="active"
+    {{ $attributes->merge(['class' => 'flex flex-row justify-around flex-auto items-end border-b border-section-separator text-sm text-subtitle overflow-x-auto overflow-y-hidden']) }}
+>
     {{ $slot }}
 </div>
--- src/resources/views/components/tab/item.blade.php
+++ src/resources/views/components/tab/item.blade.php
@@ -1,9 +1,11 @@
-@props(['label', 'active' => false])
+@props(['name', 'label'])
 
-<a {{ $attributes->merge(['class' => 'flex justify-center flex-none py-1.5'])->class([
-    'border-b-2 border-accent text-accent font-medium' => $active,
-    'border-b-2 border-transparent hover:text-on-section cursor-pointer' => !$active
-]) }}>
+<a
+    x-data="{ name: @js($name) }"
+    class="flex justify-center flex-none py-1.5"
+    x-bind:class="{ 'border-b-2 border-accent text-accent font-medium': name == active, 'border-b-2 border-transparent hover:text-on-section cursor-pointer': name != active }"
+    x-on:click="active = name"
+>
     <div class="py-1.5 px-4 hover:bg-accent/10 rounded-lg transition">
         {{ $label }}
     </div>
